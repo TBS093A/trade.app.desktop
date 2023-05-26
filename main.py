28d34f2b508dd6bf2039fd6e44d24f280bdb6479
TBS093A <zukkamil.44@gmail.com>
2023-05-26 23:42:13 +0200

    feat(docs): add github documentation plotext link

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from functionality.candlestick_chart import (
     AbstractCandlestickChart
 )
 
+# https://github.com/piccolomo/plotext/blob/master/readme/aspect.md
+
 google = AbstractCandlestickChart()
-# google.show_plot_ordinary()
 google.show_plot_with_indicators()
 
 
